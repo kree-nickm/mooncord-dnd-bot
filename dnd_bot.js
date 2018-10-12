623f9eeb298f44851e0a6dd508f393761f29ae48
Nick <38154033+kree-nickm@users.noreply.github.com>
2018-10-12 13:20:27 -0500

    Applications code moved to separate file
    Code for applications is now loaded via require(). When MySQL is implemented, switching from one to the other should be as easy as requiring the MySQL file instead of the Google Sheets file, but we'll see.

--- dnd_bot.js
+++ dnd_bot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const config = require("./config.json");
-const GoogleSpreadsheet = require('google-spreadsheet');
-const creds = require('./credentials.json');
+const Applications = require('./GoogleSheetsApplications.js');
 /*
 config.json should be saved in the same directory as this file and contain something like this:
 {
@@ -17,121 +15,10 @@ config.json should be saved in the same directory as this file and contain somet
 	"handle_column": "discordhandlecolumnheader"
 }
 */
+const client = new Discord.Client();
+const appList = new Applications(config.google_sheet, config.sheet_id, config.handle_column);
+
 var guild;
-
-function GoogleSheetsApplications(spreadsheet_id, worksheet_id, handle_column, credentials)
-{
-	this.spreadsheet_id = spreadsheet_id;
-	this.worksheet_id = worksheet_id;
-	this.handle_column = handle_column;
-	this.doc;
-	this.sheet;
-	this.all_apps;
-	this.loadApplications = function(callback)
-	{
-		if(this.sheet == null)
-		{
-			console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Cannot load application list because the worksheet was never loaded.");
-			if(typeof(callback) == "function")
-				callback(false);
-		}
-		else
-		{
-			this.sheet.getRows({orderby:this.handle_column}, (function(err,info){
-				if(err != null)
-				{
-					console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Unable to read list of applications: ", err);
-					if(typeof(callback) == "function")
-						callback(false);
-				}
-				else if(!Array.isArray(info) || !info.length)
-				{
-					console.warn("\x1b[1mGoogleSheetsApplications Warning:\x1b[0m No applications found.");
-					if(typeof(callback) == "function")
-						callback(false);
-				}
-				else if(info[0][this.handle_column] == null)
-				{
-					console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Invalid column specified for Discord handles: \x1b[1m%s\x1b[0m", this.handle_column);
-					if(typeof(callback) == "function")
-						callback(false);
-				}
-				else
-				{
-					this.all_apps = info;
-					console.log("GoogleSheetsApplications: Application list successfully read into memory.");
-					if(typeof(callback) == "function")
-						callback(true);
-				}
-			}).bind(this));
-		}
-	};
-	this.findAppByHandle = function(handle, rowSet)
-	{
-		if(rowSet == null)
-			rowSet = this.all_apps;
-		if(rowSet == null)
-			return false;
-		if(rowSet.length == 0)
-			return false;
-		var i = Math.floor(rowSet.length/2);
-		var comp = handle.toLowerCase().localeCompare(rowSet[i][this.handle_column].toLowerCase());
-		if(comp < 0)
-			return this.findAppByHandle(handle, rowSet.slice(0, i));
-		else if(comp > 0)
-			return this.findAppByHandle(handle, rowSet.slice(i, rowSet.length));
-		else
-			return rowSet[i];
-	};
-	
-	if(this.spreadsheet_id == null)
-		console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m No Google Sheets identifier specified.");
-	else if(this.worksheet_id == null)
-		console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m No worksheet identifier specified.");
-	else if(this.handle_column == null)
-		console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m No handle column identifier specified.");
-	else if(credentials == null || typeof(credentials) != "object")
-		console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m No credentials specified.");
-	else
-	{
-		this.doc = new GoogleSpreadsheet(this.spreadsheet_id);
-		this.doc.useServiceAccountAuth(credentials, (function(err){
-			if(err != null)
-				console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Unable to authenticate with Google Sheets due to invalid credentials. %s", err.message);
-			else
-			{
-				this.doc.getInfo((function(err,info){
-					if(err != null)
-						console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Unable to fetch data from Google Sheet: \x1b[1m%s\x1b[0m; Sheet ID or credentials may be invalid.\n\x1b[1m--- BEGIN ERROR RESPONSE ---\x1b[0m\n%s\n\x1b[1m--- END ERROR RESPONSE ---\x1b[0m", this.spreadsheet_id, err);
-					else
-					{
-						//console.log(info.worksheets);
-						this.sheet = null;
-						for(var i in info.worksheets)
-						{
-							if(info.worksheets[i].id == this.worksheet_id)
-							{
-								this.sheet = info.worksheets[i];
-								console.log("GoogleSheetsApplications: Application list found.");
-								break;
-							}
-						}
-						if(this.sheet == null)
-						{
-							console.error("\x1b[31mGoogleSheetsApplications Error:\x1b[0m Application list was not found using worksheet ID: \x1b[1m%s\x1b[0m", this.worksheet_id);
-						}
-						else
-						{
-							this.loadApplications();
-						}
-					}
-				}).bind(this));
-			}
-		}).bind(this));
-	}
-}
-const appList = new GoogleSheetsApplications(config.google_sheet, config.sheet_id, config.handle_column, creds);
-
 client.on("ready", function()
 {
 	if(!Array.isArray(config.admin_ids))
@@ -157,7 +44,7 @@ client.on("message", function(message)
 	{
 		if(message.author.bot || !message.content.startsWith(config.prefix))
 			return;
-		console.log(message);
+		//console.log(message);
 		// TODO: Emote codes below, but they might not work on a bot. If not, edit all the replies to get rid of them.
 		// <:moon2T:284219508615413761> <:moon2S:496519208549613568> <:moon2N:497606808542642176> <:moon2PH:482219761699389450> <:moon2A:430810259620364309>
 		let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
